0189cfc87a045e9de7c01680a2bd0f272868ae62
“Peter <petervlad88@yahoo.com>
2020-12-09 16:33:43 +0200

    Tema-introducere in java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,7 +5,33 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
 
-        System.out.println("Hello Vlad");
-        System.out.println("Hello Vlad");
+
+        System.out.println("Hello");
+        System.out.println("Vlad");
+
+        byte myByte=5+4;
+        System.out.println(myByte);
+
+        byte myByte2=9/3;
+        System.out.println(myByte2);
+
+       byte ec1=-5 + 8 * 6;
+        System.out.println(ec1);
+
+        byte ec2=(55+9) % 9;
+        System.out.println(ec2);
+
+        float ec3=20 +(float) -3*5 / 8;
+        System.out.println(ec3);
+
+        float ec4=5 + 15 / 3 * 2 - 8 % 3;
+        System.out.println(ec4);
+
+
+
+
+
+
+
     }
 }
