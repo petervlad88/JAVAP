214c9ff1c919d7cdc86e16b2066db42553646755
“Peter <petervlad88@yahoo.com>
2020-12-14 17:06:27 +0200

    Java "Tema obiecte"

--- src/Lab2/Lab2.java
+++ src/Lab2/Lab2.java
@@ -15,6 +15,17 @@ public class Lab2 {
         float result1=divide(15f,8f);
         System.out.println(result1);
 
+        int home=minus(18,4);
+        System.out.println(home);
+
+        home=multi(4,5);
+        System.out.println(home);
+
+        float home3=averageNo(4,5,10,5);
+        System.out.println(home3);
+
+
+
     }
     static int sum(int firstNumber, int secondNumber){
         return (firstNumber + secondNumber);
@@ -23,14 +34,44 @@ public class Lab2 {
     static int divide(int firstNumber,int secondNumber){
         return (firstNumber/secondNumber);
 
-    }
-    static int modulo(int firstNumber,int secondNumber){
-        return (firstNumber%secondNumber);
 
     }
     static float divide(float firstNumber,float secondNumber){
         return(firstNumber/secondNumber);
     }
+    static int minus(int firstNumber,int secondNumber){
+        return(firstNumber-secondNumber);
+
+    }
+    static int multi(int firstNumber,int secondNumber){
+        return(firstNumber * secondNumber);
+    }
+
+    public void modelJava (String[] args) {
+        System.out.println("       J    a   v     v  a ");
+        System.out.println("       J   a a   v   v  a a");
+        System.out.println("    J  J  aaaaa   V V  aaaaa");
+        System.out.println("     JJ  a     a   V  a     a");
+
+
+
+    }
+    static float averageNo (float nr1,float nr2,float nr3 ,float nr4){
+        return(nr1+nr2+nr3)/nr4;
+    }
+    public void cupFace (String[] args) {
+
+
+
+    }
+    static int modulo(int firstNumber,int secondNumber){
+        return (firstNumber%secondNumber);
+    }
+
+
+
+
+
 
 
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,22 +3,39 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        // write your code here
+
+        int ecMain=main1(5,4);
+        System.out.println(ecMain);
+
+        int ecMain2=main2(9,3);
+        System.out.println(ecMain2);
+
+        int ecMain3=main3(-5,8,6);
+        System.out.println(ecMain3);
+
+        int ecMain4=main4(55,9,9);
+        System.out.println(ecMain4);
+
+        float ecMain5=main5(20,-3,5,8);
+        System.out.println(ecMain5);
+
+
 
 
         System.out.println("Hello");
         System.out.println("Vlad");
 
-        byte myByte=5+4;
+        int myByte=5+4;
         System.out.println(myByte);
 
-        byte myByte2=9/3;
+        int myByte2=9/3;
         System.out.println(myByte2);
 
-        byte ec1=-5 + 8 * 6;
+        int ec1=-5 + 8 * 6;
         System.out.println(ec1);
 
-        byte ec2=(55+9) % 9;
+        int ec2=(55+9) % 9;
         System.out.println(ec2);
 
         float ec3=20 +(float) -3*5 / 8;
@@ -27,15 +44,25 @@ public class Main {
         float ec4=5 + 15 / 3 * 2 - 8 % 3;
         System.out.println(ec4);
 
-
-
-
-
-
-
-
-
-
-
     }
+    static int main1 (int number1,int number2 ){
+        return(number1+number2);
+    }
+    static int main2 (int number1,int number2){
+        return (number1/number2);
+    }
+    static int main3 (int number1,int number2,int number3 ){
+        return(number1 + number2 * number3);
+    }
+    static int main4 (int number1,int number2,int number3 ) {
+        return (number1 + number2 % number3);
+    }
+    static float main5 (float number1,float number2,float number3,float number4 ) {
+        return (number1 + number2 * number3 / number4);
+    }
+    static float main6 (float number1,float number2,float number3,float number4 ,float number5) {
+        return (number1 + number2 / number3 * number4 % number5);
+    }
+
+
 }
